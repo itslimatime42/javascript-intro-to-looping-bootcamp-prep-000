0abc96ce739292d51fa12e8433ebe63cb1e60343
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 14:02:34 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    var stringToAdd;
+    if (i === 1) {
+      stringToAdd = "I am 1 strange loop.";
+      array.push(stringToAdd);
+    } else {
+      stringToAdd = `I am ${i} strange loops.`;
+      array.push(stringToAdd);
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  console.log(n);
+  while (n > 0) {
+    console.log(--n);
+  }
+}
+
+whileLoop(13);
